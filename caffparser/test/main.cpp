45628f39f4138a4711720cf2951bc69b0b82ac7c
schdwlk <dscharnitzky@hotmail.com>
2020-11-01 14:43:30 +0100

    some test

--- caffparser/test/main.cpp
+++ caffparser/test/main.cpp
@@ -1,6 +1,13 @@
 #include <parser.hpp>
+#include <base.hpp>
 
 int main() {
   print("Hello, makefile!\n");
+  std::vector<std::byte> byteCaff;
+  byteCaff.push_back(std::byte(0));
+  auto ret = parse(byteCaff, false);
+  if(ret[0] == std::byte(ERROR)) {
+    print("Test OK!\n");
+  }
   return 0;
 }
