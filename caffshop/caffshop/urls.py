c0756d2f566caccb551a3f9ccba4dca144feb006
Heszler András Barnabás <heszler.andras@gmail.com>
2020-11-22 17:47:33 +0100

    Create login and logout functions

--- caffshop/caffshop/urls.py
+++ caffshop/caffshop/urls.py
@@ -24,11 +24,14 @@ urlpatterns += [
     path('webshop/', include('webshop.urls')),
 ]
 #Add URL maps to redirect the base URL to our application
-from django.views.generic import RedirectView
-urlpatterns += [
-    path('', RedirectView.as_view(url='webshop/', permanent=True)),
-]
+# from django.views.generic import RedirectView
+# urlpatterns += [
+#     path('', RedirectView.as_view(url='webshop/', permanent=True)),
+# ]
 from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += [
+    path('', include('django.contrib.auth.urls')),
+]
