7a643baf6b491fb099a115103e096d9ae519ab9b
Heszler András Barnabás <heszler.andras@gmail.com>
2020-11-28 20:10:10 +0100

    Edit base_generic navbar

--- caffshop/webshop/urls.py
+++ caffshop/webshop/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.CAFFListView.as_view()),
+    path('', views.CAFFListView.as_view(), name='webshop'),
     path('upload/', views.upload_caff, name='caff-upload'),
 ]
